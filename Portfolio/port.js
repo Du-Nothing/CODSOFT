619c82da605ef70d4df3dbb0b294feafbfb39f53
Du-Nothing <128450855+Du-Nothing@users.noreply.github.com>
2023-07-31 17:50:30 +0530

    Update port.js
    ddddd

--- Portfolio/port.js
+++ Portfolio/port.js
@@ -1,3 +1,27 @@
+// active header
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+    };
+  });
+};
+
+
+//Typewriter
+
 var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
@@ -67,21 +91,33 @@ icon.onclick = function(){
     }
 }
 
-// form
-// function sendEmail(){
-//   sendEmail.send({
-//     Host : "smtp.gmail.com",
-//     Username : "takeandfake12@gmail.com",
-//     Password : "23762308",
-//     To : 'takeandfake12@gmail.com',
-//     From : document.getElementById("email").value,
-//     Subject : "New Contact Form Enquiry",
-//     Body : "Name: " + document.getElementById("name").value
-//     + "<br> Email: " + document.getElementById("email").value
-//     + "<br> Phone no: " + document.getElementById("phone").value
-//     + "<br> Message: " + document.getElementById("message").value
-//   }).then(
-//     message => alert("Message sent successfully")
-//   );
-// }
+//Toggle
+
+const menuBtn = document.querySelector(".menu-btn");
+const nav = document.querySelector(".nav");
+
+menuBtn.addEventListener("click",() => {
+menuBtn.classList.toggle("active");
+nav.classList.toggle("active");
+});
+
+   
+// Form
+function sendEmail(){
+  Email.send({
+   SecureToken: "927f72f3-3795-447f-9d34-24f325dadc63",
+   Host : "smtp.elasticemail.com",
+   Username : "..@gmail.com",
+   Password : ".....",
+   To : '...@gmail.com',
+   From : document.getElementById("email").value,
+   Subject : "New Contact Form Enquiry",
+   Body : "Name: " + document.getElementById("name").value
+   + "<br> Email: " + document.getElementById("email").value
+   + "<br> Phone no: " + document.getElementById("phone").value
+   + "<br> Message: " + document.getElementById("message").value
+   }).then(
+   message => alert("Message sent successfully")    
+   );
+   }
 
