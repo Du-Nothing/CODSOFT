f3693a7c5e47fe79046c7b3904f993cc29ce78fd
Du-Nothing <128450855+Du-Nothing@users.noreply.github.com>
2023-08-07 16:33:45 +0530

    Add files via upload
    ffhgtry

---
+++ Calculator/port.js
@@ -0,0 +1,29 @@
+// <-------------All functions-------------->
+function calcNumbers(result)
+{
+    document.querySelector('.area').value += result;
+}
+function percentage(result)
+{
+    var result1 = String(result);
+    if(result1.includes("%"))
+    {
+        var a, b , percent;
+        var splitWord = [];
+        a = Number(splitWord[0]);
+        b = Number(splitWord[1]);
+        percent = (a/100 * b);
+        document.querySelector('.area').value = percent;
+    }
+}
+function makeNegative(result)
+{
+    if(result.charAt(0)=='_')
+    {
+        document.querySelector('.area').value = result.replace('-','');
+    }
+    else
+    {
+        document.querySelector('.area').value = '-' + result;
+    }
+}
